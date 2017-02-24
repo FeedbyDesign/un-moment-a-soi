071a0de0f694fddef813508d645de9643b6c7f19
Marc Coet <marcoet@gmail.com>
2017-02-24 18:21:28 +0100

    optimized pictures. deleted html form

--- src/Intro.js
+++ src/Intro.js
@@ -14,9 +14,9 @@ const Intro = () => {
       <img className="img_wide" src={Intro_img_wide} alt="salon" />
       <div id="intro_photosFour">
         <img className="img_1-4" src={img_14_1} alt="salon" />
-        <img className="img_1-4" src={img_14_2} alt="salon" />
-        <img className="img_1-4" src={img_14_3} alt="salon" />
+        <img className="img_1-4 disapear600" src={img_14_2} alt="salon" />
         <img className="img_1-4" src={img_14_4} alt="salon" />
+        <img className="img_1-4 disapear600" src={img_14_3} alt="salon" />
       </div>
     </section>
   )
