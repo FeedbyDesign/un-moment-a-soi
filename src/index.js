e3a0b79d7b7092183c5919dc869444da4bf85202
MarcCoet <marcoet@gmail.com>
2017-03-14 15:32:03 +0000

    optimized with google assets. Included css static ressources in JS.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './css/normalize.css';
 import './css/style.css';
+import './css/fontawesome.css';
 
 ReactDOM.render(
   <App />,
